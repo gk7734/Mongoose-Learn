f5c562b8bee9f868f9a1b7980978d3fce938e906
gk7734 <gamekiller7734@naver.com>
2023-10-11 19:22:39 +0900

    백엔드 MongoDB 메서드 연결 성공

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className={`flex justify-between items-center bg-slate-800 px-8 py-3`}>
+        <nav className={`flex justify-between items-center bg-slate-800 px-8 py-3 rounded-b-xl`}>
             <Link href={"/"} className={`text-white font-bold`}>GK</Link>
             <Link href={"/addTopic"} className={`bg-white font-bold p-2 rounded-md`}>Add Topic</Link>
         </nav>
