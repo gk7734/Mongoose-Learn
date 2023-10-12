b3c03c505f7585155f4379675300b5c943fbbb62
gk7734 <gamekiller7734@naver.com>
2023-10-12 19:44:33 +0900

    프로젝트 완성 (1)

--- src/app/addTopic/page.js
+++ src/app/addTopic/page.js
@@ -8,6 +8,7 @@ export default function AddTopic() {
     const [description, setDescription] = useState('')
 
     const router = useRouter()
+    let btn = 0
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -16,20 +17,24 @@ export default function AddTopic() {
             alert("Title and description are required")
             return;
         }
-        try {
-            const res = await fetch('https://mongoose-learn.vercel.app/api/topics', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({title, description})
-            })
-            if (res.ok) {
-                router.push('/')
-                router.refresh()
-            } else {
-                throw new Error("Failed to create topic")
+        if (btn === 0) {
+            console.log(btn)
+            btn = 1;
+            try {
+                const res = await fetch('https://mongoose-learn.vercel.app/api/topics', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({title, description})
+                })
+                if (res.ok) {
+                    router.push('/')
+                    router.refresh()
+                } else {
+                    throw new Error("Failed to create topic")
+                }
+            } catch (err) {
+                console.log(err)
             }
-        } catch (err) {
-            console.log(err)
         }
     }
 
@@ -37,7 +42,7 @@ export default function AddTopic() {
         <form onSubmit={handleSubmit} className={`flex flex-col gap-3`}>
             <input onChange={(e) => setTitle(e.target.value)} value={title} type={`text`} placeholder="이름" className={`border border-slate-500 px-8 py-2`}/>
             <input onChange={(e) => setDescription(e.target.value)} value={description} type={`text`} placeholder="게시글" className={`border border-slate-500 px-8 py-2`}/>
-            <button type={"submit"} className={`bg-green-300 font-bold text-white py-3 px-6 w-fit rounded-lg hover:bg-green-600`}>추가하기</button>
+            <button type="submit" className={`bg-green-300 font-bold text-white py-3 px-6 w-fit rounded-lg hover:bg-green-600`}>추가하기</button>
         </form>
     );
 }
