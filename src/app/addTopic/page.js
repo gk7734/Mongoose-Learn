f48151d376fbd9481d58488afde2fd2ff8249ab8
gk7734 <gamekiller7734@naver.com>
2023-10-12 12:26:51 +0900

    프로젝트 완성 (1)

--- src/app/addTopic/page.js
+++ src/app/addTopic/page.js
@@ -16,14 +16,12 @@ export default function AddTopic() {
             alert("Title and description are required")
             return;
         }
-
         try {
             const res = await fetch('https://mongoose-learn.vercel.app/api/topics', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({title, description})
             })
-
             if (res.ok) {
                 router.push('/')
                 router.refresh()
@@ -31,7 +29,7 @@ export default function AddTopic() {
                 throw new Error("Failed to create topic")
             }
         } catch (err) {
-
+            console.log(err)
         }
     }
 
@@ -39,7 +37,7 @@ export default function AddTopic() {
         <form onSubmit={handleSubmit} className={`flex flex-col gap-3`}>
             <input onChange={(e) => setTitle(e.target.value)} value={title} type={`text`} placeholder="이름" className={`border border-slate-500 px-8 py-2`}/>
             <input onChange={(e) => setDescription(e.target.value)} value={description} type={`text`} placeholder="게시글" className={`border border-slate-500 px-8 py-2`}/>
-            <button type={"submit"} className={`bg-green-300 font-bold text-white py-3 px-6 w-fit rounded-lg`}>Add Topic</button>
+            <button type={"submit"} className={`bg-green-300 font-bold text-white py-3 px-6 w-fit rounded-lg hover:bg-green-600`}>추가하기</button>
         </form>
     );
 }
